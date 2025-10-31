0339fc9011692abfbe4b25fd1d8708bfd5745689
Osaidi1 <hassanosaid123@gmail.com>
2025-10-31 13:04:09 +0100

    fixed skeleton move and random idle and is now frame-independent, chnged timeline of parkour and fight

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -10,10 +10,13 @@ extends CharacterBody2D
 var rng = RandomNumberGenerator.new()
 
 var dir := 1
+var last_dir := dir
+var friction := 0.9
 var is_chasing := false
 var is_attacking := false
 var is_hurting := false
-var is_walking := true 
+var is_walking := true
+var is_stopped := false
 
 func _ready():
 	random_time()
@@ -23,27 +26,43 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	#Rotate
-	if dir > 0:
-		animater.flip_h = false
-		turn.position.x = -20
-	if dir < 0:
-		animater.flip_h = true
-		turn.position.x = 20
-	
-	#Turn
+	#Turn at wall
 	if turn.is_colliding():
 		dir *= -1
 	
 	#Add Velocity
-	velocity.x = SPEED * dir
+	if is_walking:
+		velocity.x = (SPEED * dir * delta) * 60
+	elif is_stopped:
+		velocity.x *= (friction * delta) * 16
 	
-	#Move
+	#Call Funcs
+	facing_dir()
+	
+	anims()
+	
 	move_and_slide()
 
+func facing_dir():
+	if dir != last_dir:
+		animater.flip_h = dir < 0
+		turn.position.x *= -1
+		last_dir = dir
+
 func random_time():
-	timer.wait_time = rng.randf_range(2, 5)
+	timer.wait_time = rng.randf_range(4.0, 7.0)
+	is_walking = true
+	is_stopped = false
 
 func timer_end() -> void:
+	is_walking = false
+	is_stopped = true
+	await get_tree().create_timer(rng.randf_range(1.0, 2.5)).timeout
 	random_time()
-	dir *= -1
+	timer.start()
+
+func anims():
+	if is_walking:
+		animater.play("walk")
+	elif is_stopped:
+		animater.play("idle")
